a01e97e6c095813fa257cb367c621e13b6f37569
Calen Pennington <cale@edx.org>
2016-08-23 10:43:22 -0400

    fixup! Make openedx.yaml process idempotent, so that it can be resumable

--- edx_repo_tools/oep2/explode_repos_yaml.py
+++ edx_repo_tools/oep2/explode_repos_yaml.py
@@ -10,10 +10,18 @@ import yaml
 
 from edx_repo_tools.auth import pass_github
 
+logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
 
 BRANCH_NAME = 'add-openedx-yaml'
 OPEN_EDX_YAML = 'openedx.yaml'
+
+
+def dry_echo(dry, message, *args, **kwargs):
+    click.echo("{dry}{message}".format(
+        dry=click.style("DRY RUN - SKIPPED: ", fg='yellow', bold=True) if dry else "",
+        message=click.style(message, *args, **kwargs)
+    ))
 
 
 @click.command()
@@ -33,6 +41,8 @@ def explode(hub, dry):
     repos_yaml = repo_tools_data.contents('repos.yaml').decoded
 
     repos = yaml.safe_load(repos_yaml)
+
+    dry_text = click.style("DRY RUN - SKIPPED: ", fg='yellow', bold=True) if dry else ""
 
     for repo, repo_data in repos.items():
         user, _, repo_name = repo.partition('/')
@@ -54,7 +64,7 @@ def explode(hub, dry):
             # http://open-edx-proposals.readthedocs.io/en/latest/oeps/oep-0002.html#specification
 
             {}
-        """).format(file_contents).strip()
+        """).format(file_contents).strip() + "\n"
 
         gh_repo = hub.repository(user, repo_name)
 
@@ -63,13 +73,14 @@ def explode(hub, dry):
             continue
 
         try:
-            parent_commit = gh_repo.default_branch.commit.sha
+            parent_commit = gh_repo.branch(gh_repo.default_branch).commit.sha
         except:
             LOGGER.warning(
                 "No commit on default branch %s in repo %s",
                 gh_repo.default_branch,
                 gh_repo.full_name
             )
+            continue
 
         if not dry:
             if gh_repo.branch(BRANCH_NAME) is None:
@@ -78,20 +89,49 @@ def explode(hub, dry):
                     parent_commit
                 )
 
-            gh_repo.create_file(
-                path=OPEN_EDX_YAML,
-                message='Add an OEP-2 compliant openedx.yaml file',
-                content=file_contents,
-                branch=BRANCH_NAME,
-            )
-        else:
-            click.secho(
-                'Would have created openedx.yaml file on branch {} in {}:'.format(
-                    BRANCH_NAME, gh_repo.full_name,
+        contents = gh_repo.contents(OPEN_EDX_YAML, ref=BRANCH_NAME)
+
+        if contents is None:
+            dry_echo(
+                dry,
+                "Creating openedx.yaml file on branch {repo}:{branch}".format(
+                    repo=gh_repo.full_name,
+                    branch=BRANCH_NAME,
                 ),
                 fg='green',
             )
-            click.secho(file_contents, fg='yellow')
+            click.secho(file_contents, fg='blue')
+            if not dry:
+                try:
+                    gh_repo.create_file(
+                        path=OPEN_EDX_YAML,
+                        message='Add an OEP-2 compliant openedx.yaml file',
+                        content=file_contents,
+                        branch=BRANCH_NAME,
+                    )
+                except TypeError:
+                    # Sadly, TypeError means there was a permissions issue...
+                    LOGGER.exception("Unable to create openedx.yaml")
+                    continue
+        else:
+            if contents.decoded != file_contents:
+                dry_echo(
+                    dry,
+                    "Updated openedx.yaml file on branch {repo}:{branch}".format(
+                        repo=gh_repo.full_name,
+                        branch=BRANCH_NAME,
+                    ),
+                    fg='green',
+                )
+                click.secho(file_contents, fg='blue')
+                if not dry:
+                    gh_repo.update_file(
+                        path=OPEN_EDX_YAML,
+                        message='Add an OEP-2 compliant openedx.yaml file',
+                        content=file_contents,
+                        branch=BRANCH_NAME,
+                        sha=contents.sha if contents is not None else None,
+                    )
 
         pr_body = textwrap.dedent("""
             This adds an `openedx.yaml` file, as described by OEP-2:
@@ -100,30 +140,54 @@ def explode(hub, dry):
             The data in this file was transformed from the contents of
             edx/repo-tools-data:repos.yaml
         """)
-        pr_title = 'Add an OEP-2 compliant openedx.yaml file',
+        pr_title = 'Add an OEP-2 compliant openedx.yaml file'
 
         existing_pr = [
             pr
             for pr
             in gh_repo.iter_pulls(
-                head='edx/{}'.format(BRANCH_NAME),
+                head='edx:{}'.format(BRANCH_NAME),
                 state='open'
             )
         ]
 
         if existing_pr:
             pull = existing_pr[0]
-            if not dry:
-                pull.update(
-                    title=pr_title,
-                    body=pr_body,
+            if pull.title != pr_title or pull.body != pr_body:
+                dry_echo(
+                    dry,
+                    textwrap.dedent("""\
+                        Updated pull request {repo}#{number}: {title}
+                            URL: {url}
+                    """).format(
+                        url=pull.html_url,
+                        repo=gh_repo.full_name,
+                        number=pull.number,
+                        title=pull.title,
+                    ),
+                    fg='green'
                 )
-            click.secho('{}pdated pull request {} against {}'.format(
-                'Would have u' if dry else 'U',
-                pull.html_url,
-                repo,
-            ), fg='green')
+
+                if not dry:
+                    pull.update(
+                        title=pr_title,
+                        body=pr_body,
+                    )
         else:
+            dry_echo(
+                dry,
+                textwrap.dedent("""\
+                    Created pull request {repo}#{number}: {title}
+                        URL: {url}
+                """).format(
+                    url=pull.html_url if not dry else "N/A",
+                    repo=gh_repo.full_name,
+                    number=pull.number if not dry else "XXXX",
+                    title=pull.title if not dry else pr_title,
+                ),
+                fg='green'
+            )
+
             if not dry:
                 pull = gh_repo.create_pull(
                     title=pr_title,
@@ -131,11 +195,6 @@ def explode(hub, dry):
                     head=BRANCH_NAME,
                     body=pr_body
                 )
-            click.secho('{}reated pull request {} against {}'.format(
-                'Would have c' if dry else 'C',
-                pull.html_url if not dry else "N/A",
-                repo,
-            ), fg='green')
 
 
 @click.command()
